e71b204bb110fa06f23217163904dbbaa369181c
Jesvinvinoj <141458150+Jesvinvinoj@users.noreply.github.com>
2023-08-12 00:07:35 -0400

    footer.js

--- footer.js
+++ footer.js
@@ -1,7 +1,7 @@
 // footer.js
 document.addEventListener("DOMContentLoaded", function () {
     const footer = document.createElement("footer");
-    footer.innerHTML = "<p>&copy; " + new Date().getFullYear() + " Aadish DragonStrike Board Game. All rights reserved.</p>";
+    footer.innerHTML = "<p>&copy; " + new Date().getFullYear() + "  jesvin laser  light active Game. All rights reserved.</p>";
     document.body.appendChild(footer);
   });
   
